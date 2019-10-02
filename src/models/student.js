a5742271e5f21f7704f4d830d04c980c31966029
Rurick Maqueo <skintigthvader@gmail.com>
2019-10-02 00:56:03 -0500

    ADD: unique characteristic to email propertie

--- src/models/student.js
+++ src/models/student.js
@@ -18,6 +18,7 @@ const studentSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
     pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ // eslint-disable-line no-useless-escape
   },
   age: {
