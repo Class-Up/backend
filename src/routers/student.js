cad9609f9a6d149eb986c0e54c6e7a633ce5ba19
Rurick Maqueo <skintigthvader@gmail.com>
2019-10-02 01:13:23 -0500

    ADD:  get /students/ endpoint

---
+++ src/routers/student.js
@@ -0,0 +1,32 @@
+
+const express = require('express')
+
+const student = require('../usecases/student')
+
+const auth = require('../middlewares/auth')
+
+const router = express.Router()
+
+router.get('/', auth, async (request, response) => {
+  try {
+    const allStudents = await student.getAll()
+    response.json({
+      success: true,
+      messaje: 'All students',
+      data: {
+        students: allStudents
+      }
+    })
+  } catch (error) {
+    response.status(401)
+    response.json({
+      success: false,
+      message: 'something failed',
+      data: {
+        error: error.message
+      }
+    })
+  }
+})
+
+module.exports = router
