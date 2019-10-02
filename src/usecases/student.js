c9cf8867aa67de07c5d112457c1461d2f7ffde55
Rurick Maqueo <skintigthvader@gmail.com>
2019-10-02 01:00:47 -0500

    ADD: login usecase

--- src/usecases/student.js
+++ src/usecases/student.js
@@ -1,5 +1,6 @@
 
 const bcrypt = require('../lib/bcrypt')
+const jwt = require('../lib/jwt')
 
 const Student = require('../models/student')
 
@@ -34,10 +35,21 @@ function updateById (id, newData) {
   return Student.findByIdAndUpdate(id, newData)
 }
 
+async function login (email, password) {
+  const studentFound = await Student.findOne({ email })
+  if (!studentFound) throw new Error('email or password wrong')
+
+  const isPasswordCorrect = await bcrypt.compare(password, studentFound.password)
+  if (!isPasswordCorrect) throw new Error('email or password wrong')
+
+  return jwt.sign({ id: studentFound.id })
+}
+
 module.exports = {
   create,
   getAll,
   getById,
   deleteById,
-  updateById
+  updateById,
+  login
 }
