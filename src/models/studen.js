c66c682d756476cd01029d5aee22e9ca4591a95d
Rurick Maqueo <skintigthvader@gmail.com>
2019-10-02 00:21:47 -0500

    ADD: Regex for email

--- src/models/studen.js
+++ src/models/studen.js
@@ -11,7 +11,16 @@ const studentSchema = mongoose.Schema({
     type: String,
     required: true,
     minlength: 2
+  },
+  picture: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true,
+    pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   }
+
 })
 
 module.exports = mongoose.model('Student', studentSchema)
