f05fb488c79e2ce9e1bf1a5bf1d2afcd04089c0a
brendag <brennnda.gonzalezq@gmail.com>
2019-10-05 18:15:14 -0500

    added score usercase

---
+++ src/usecases/scores.js
@@ -0,0 +1,32 @@
+
+const Score = require('../models/score')
+
+function create({ value }) {
+  return Score.create({
+    value
+  })
+}
+
+function getAll() {
+  return Score.find()
+}
+
+function getById(id) {
+  return Score.findById(id)
+}
+
+function updateById(id, newData) {
+  return Score.findByIdAndUpdate(id, newData)
+}
+
+function deleteById(id) {
+  return Score.findByIdAndDelete(id)
+}
+
+module.exports = {
+  create,
+  getAll,
+  getById,
+  updateById,
+  deleteById
+}
