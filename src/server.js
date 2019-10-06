cbf88f450a01ee2ae80560183156333564252a7d
brendag <brennnda.gonzalezq@gmail.com>
2019-10-06 16:30:41 -0500

    add personalities router to server

--- src/server.js
+++ src/server.js
@@ -2,11 +2,12 @@
 const express = require('express')
 const cors = require('cors')
 
-const studentRouter = require('./routers/student')
+const studentRouter = require('./routers/students')
 const medalsRouter = require('./routers/medals')
 const notesRouter = require('./routers/notes')
 const scoresRouter = require('./routers/scores')
 const topicsRouter = require('./routers/topics')
+const personalitiesRouter = require('./routers/personalities')
 
 const app = express()
 
@@ -18,6 +19,7 @@ app.use('/notes', notesRouter)
 app.use('/medals', medalsRouter)
 app.use('/scores', scoresRouter)
 app.use('/topics', topicsRouter)
+app.use('/personalities', personalitiesRouter)
 
 app.get('/', (request, response) => {
   response.json({
