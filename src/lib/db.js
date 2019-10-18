3149a6b09d8820f679e67b1dbf9ccd8fe4d3abb5
Rurick Maqueo <skintigthvader@gmail.com>
2019-10-18 10:22:02 -0500

    test cfenv

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,8 @@
 
 const mongoose = require('mongoose')
 
+const cfenv = require('cfenv')
+
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_HOST = process.env.DB_HOST
@@ -14,6 +16,14 @@ console.log('DB_USER:', DB_USER)
 console.log('DB_PASSWORD:', DB_PASSWORD)
 console.log('DB_HOST:', DB_HOST)
 console.log('DB_NAME:', DB_NAME)
+
+const appEnv = cfenv.getAppEnv()
+const appSer = cfenv.getServices()
+
+console.log('------------------------------------------------------')
+console.log('App Env:', appEnv)
+console.log('App Ser:', appSer)
+console.log('------------------------------------------------------')
 
 module.exports = () => mongoose.connect(
   uri,
