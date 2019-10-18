47bc1f4ef1a31a0ffc653d1452924d804c802d8c
Rurick Maqueo <skintigthvader@gmail.com>
2019-10-18 02:35:40 -0500

    try to get env

--- src/lib/db.js
+++ src/lib/db.js
@@ -6,7 +6,14 @@ const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_HOST = process.env.DB_HOST
 const DB_NAME = process.env.DB_NAME
 
-const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retry=true&w=majority`
+// const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retry=true&w=majority`
+const uri = 'mongodb+srv://ibm_cf:KKHbf7X1UIR99ks3@classup-hofdh.mongodb.net/classup?retry=true&w=majority'
+
+console.log('Process.env:', process.env)
+console.log('DB_USER:', DB_USER)
+console.log('DB_PASSWORD:', DB_PASSWORD)
+console.log('DB_HOST:', DB_HOST)
+console.log('DB_NAME:', DB_NAME)
 
 module.exports = () => mongoose.connect(
   uri,
