f0fbe303f14fd55b6423faa165d1570a93fbd196
Rurick Maqueo <skintigthvader@gmail.com>
2019-10-17 22:09:50 -0500

    hardcodded db uri

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,14 +1,16 @@
 
 const mongoose = require('mongoose')
 
-const {
-  DB_USER,
-  DB_PASSWORD,
-  DB_HOST,
-  DB_NAME
-} = process.env
+// const {
+//   DB_USER,
+//   DB_PASSWORD,
+//   DB_HOST,
+//   DB_NAME
+// } = process.env
 
-const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retry=true&w=majority`
+// const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retry=true&w=majority`
+
+const uri = 'mongodb+srv://skintigth:SkinMONGO94@classup-hofdh.mongodb.net/classup?retry=true&w=majority'
 
 module.exports = () => mongoose.connect(
   uri,
