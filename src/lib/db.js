c646095d9c920ae764bbe1f42c44633c04a53bec
Rurick Maqueo <skintigthvader@gmail.com>
2019-10-18 11:30:22 -0500

    use custom buildpack

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,10 +1,12 @@
 
 const mongoose = require('mongoose')
 
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_HOST = process.env.DB_HOST
-const DB_NAME = process.env.DB_NAME
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_NAME
+} = process.env.DB_NAME
 
 const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retry=true&w=majority`
 
