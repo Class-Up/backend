12593d2ce3fb0ce4ba524038cd00e97ed6683406
Rurick Maqueo <skintigthvader@gmail.com>
2019-10-20 16:14:50 -0500

    created group model

---
+++ src/models/groups.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const taskSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  students: {
+    type: [{
+      type: mongoose.Schema.ObjectId,
+      ref: 'Student'
+    }]
+  },
+  tasks: {
+    type: [{
+      type: mongoose.Schema.ObjectId,
+      ref: 'Task'
+    }]
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false
+  },
+  dateCreated: {
+    type: Date,
+    default: new Date()
+  },
+  dateUpdated: {
+    type: Date,
+    default: new Date()
+  }
+})
+
+module.exports = mongoose.model('Task', taskSchema)
