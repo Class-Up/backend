42017817fb96b84ead2fcd6cb21890f36468958f
Rurick Maqueo <skintigthvader@gmail.com>
2019-10-24 22:22:52 -0500

    creted all the use case for the groups

--- src/usecases/groups.js
+++ src/usecases/groups.js
@@ -1,12 +1,25 @@
 
 const Group = require('../models/groups')
+const Student = require('../models/students')
 
-function create ({ name, topic, students }) {
+function create ({ name, topic, students, teacher }) {
   return Group.create({
     name,
     topic,
-    students
+    students,
+    teacher
   })
+}
+
+function getById (groupId) {
+  return Group.findById(groupId)
+    .populate('students')
+    .populate('tasks')
+    .populate('teacher')
+}
+
+function getAll () {
+  return Group.find()
 }
 
 function getAllStudents (groupId) {
@@ -15,6 +28,10 @@ function getAllStudents (groupId) {
 
 function getAllTasks (groupId) {
   return Group.findById(groupId).populate('tasks')
+}
+
+function getTeacher (groupId) {
+  return Group.findById(groupId).populate('teacher')
 }
 
 async function addStudent (groupId, newStudentId) {
@@ -27,9 +44,44 @@ async function addStudent (groupId, newStudentId) {
   return Group.findOneAndUpdate(groupId, { students })
 }
 
+function makePublic ({ groupId }) {
+  const signCode = Math.floor((Math.random() * 1000) + 1)
+
+  return Group.findByIdAndUpdate(groupId, { signCode })
+}
+
+async function enroll ({ studentId, signCode }) {
+  const groupFound = await Group.findOne({ signCode })
+  const studentFound = await Student.findById(studentId)
+
+  const students = [
+    ...groupFound.students,
+    studentFound._id
+  ]
+
+  const groupUpdated = await Group.findByIdAndUpdate(groupFound._id, { students })
+
+  const groups = [
+    ...studentFound.groups,
+    groupFound._id
+  ]
+
+  const studentUpdated = await Student.findByIdAndUpdate(studentFound._id, { groups, tasks: groupFound.tasks })
+
+  return {
+    student: studentUpdated,
+    group: groupUpdated
+  }
+}
+
 module.exports = {
   create,
+  getById,
+  getAll,
+  getTeacher,
   getAllTasks,
   getAllStudents,
-  addStudent
+  addStudent,
+  makePublic,
+  enroll
 }
