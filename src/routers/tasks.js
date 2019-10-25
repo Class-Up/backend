fa4fbad552e7e2463c7bda3472e4a21210e3aff5
Rurick Maqueo <skintigthvader@gmail.com>
2019-10-24 22:19:47 -0500

    ADD: error handling

--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -2,11 +2,9 @@ const express = require('express')
 
 const task = require('../usecases/tasks')
 
-const auth = require('../middlewares/auth')
-
 const router = express.Router()
 
-router.get('/', auth, async (request, response) => {
+router.get('/', async (request, response) => {
   try {
     const allTasks = await task.getAll()
     response.json({
@@ -22,13 +20,15 @@ router.get('/', auth, async (request, response) => {
       success: false,
       message: 'something failed',
       data: {
-        error: error.message
+        error: error.message,
+        endPoint: 'tasks.get',
+        route: '/'
       }
     })
   }
 })
 
-router.get('/:id', auth, async (request, response) => {
+router.get('/:id', async (request, response) => {
   try {
     const { id } = request.params
     const taskFound = await task.getById(id)
@@ -45,13 +45,15 @@ router.get('/:id', auth, async (request, response) => {
       success: false,
       message: 'something failed',
       data: {
-        error: error.message
+        error: error.message,
+        endPoint: 'tasks.get',
+        route: '/:id'
       }
     })
   }
 })
 
-router.get('/:studentId', auth, async (request, response) => {
+router.get('/:studentId', async (request, response) => {
   try {
     const { studentId } = request.params
     const studentTasks = await task.getManyByStudentId(studentId)
@@ -68,18 +70,21 @@ router.get('/:studentId', auth, async (request, response) => {
       success: false,
       message: 'something failed',
       data: {
-        error: error.message
+        error: error.message,
+        endPoint: 'tasks.get',
+        route: '/:studentId'
       }
     })
   }
 })
 
-router.post('/', auth, async (request, response) => {
+router.post('/', async (request, response) => {
   try {
-    const newTask = await task.create(request.body)
+    const { groupId, taskData } = request.body
+    const newTask = await task.create(groupId, taskData)
     response.json({
       success: true,
-      message: 'Task Created',
+      message: `Task Created and added to group ${groupId}`,
       data: {
         task: newTask
       }
@@ -90,13 +95,15 @@ router.post('/', auth, async (request, response) => {
       success: false,
       message: 'something failed',
       data: {
-        error: error.message
+        error: error.message,
+        endPoint: 'tasks.post',
+        route: '/'
       }
     })
   }
 })
 
-router.patch('/:id', auth, async (request, response) => {
+router.patch('/:id', async (request, response) => {
   try {
     const { id } = request.params
     const { body } = request
@@ -114,13 +121,15 @@ router.patch('/:id', auth, async (request, response) => {
       success: false,
       message: 'something failed',
       data: {
-        error: error.message
+        error: error.message,
+        endPoint: 'tasks.patch',
+        route: '/:id'
       }
     })
   }
 })
 
-router.delete('/:id', auth, async (request, response) => {
+router.delete('/:id', async (request, response) => {
   try {
     const { id } = request.params
 
@@ -137,7 +146,9 @@ router.delete('/:id', auth, async (request, response) => {
     response.json({
       success: false,
       message: 'Something failed',
-      error: error.message
+      error: error.message,
+      endPoint: 'tasks.delete',
+      route: '/:id'
     })
   }
 })
